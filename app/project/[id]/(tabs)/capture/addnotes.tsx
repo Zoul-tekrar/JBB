e0dcf6f496a0f4f9424971e73b83a6276e08769f
Zulfu <zoulina801@gmail.com>
2026-03-03 11:18:55 +0000

    Refactored out add note component

--- app/project/[id]/(tabs)/capture/addnotes.tsx
+++ app/project/[id]/(tabs)/capture/addnotes.tsx
@@ -1,29 +1,27 @@
-import {
-  Category,
-  SimpleCategoryDropdown,
-} from "@/components/app-components/simpleCategoryDropdown";
+import AddNotes from "@/components/addnotes-refactor";
+import { Category } from "@/components/app-components/simpleCategoryDropdown";
 import { JbbTitle } from "@/components/design-components/JbbTitle";
 import { API_BASE_URL } from "@/constants/urls";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useLocalSearchParams } from "expo-router";
-import { Controller, useForm } from "react-hook-form";
-import { Button, Text, TextInput, View } from "react-native";
+import { useForm } from "react-hook-form";
+import { Button, View } from "react-native";
 import z from "zod";
 
-export default function AddNotes() {
+const CaptureEntryNoteSchema = z.object({
+  categoryId: z.number(),
+  shortDescription: z.string().trim().max(50).min(1),
+});
+
+export type CaptureEntry = z.infer<typeof CaptureEntryNoteSchema>;
+
+export default function Notes() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const categories: Category[] = [
     { id: 1, name: "Structural Foundation" },
     { id: 2, name: "Framing" },
     { id: 3, name: "Electrical" },
   ];
-
-  const CaptureEntryNoteSchema = z.object({
-    categoryId: z.number(),
-    shortDescription: z.string().trim().max(50).min(1),
-  });
-
-  type CaptureEntry = z.infer<typeof CaptureEntryNoteSchema>;
 
   const {
     control,
@@ -55,45 +53,11 @@ export default function AddNotes() {
   return (
     <View>
       <JbbTitle title={"Add notes"}></JbbTitle>
-      {errors.categoryId?.message && (
-        <Text className="text-center text-bold text-l text-red-700 font-semibold">
-          {errors.categoryId.message}
-        </Text>
-      )}
-      <Controller
+      <AddNotes
+        categories={categories}
         control={control}
-        name="categoryId"
-        render={({ field: { onChange, value } }) => (
-          <View style={{ gap: 6 }}>
-            <Text style={{ opacity: 0.8 }}>Category</Text>
-            <SimpleCategoryDropdown
-              categories={categories}
-              valueId={value}
-              onChange={onChange}
-            ></SimpleCategoryDropdown>
-          </View>
-        )}
-      ></Controller>
-
-      <View>
-        <Text>Notes</Text>
-        {errors.shortDescription?.message && (
-          <Text className="text-center text-bold text-l text-red-700 font-semibold">
-            {errors.shortDescription.message}
-          </Text>
-        )}
-        <Controller
-          control={control}
-          name="shortDescription"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              className="mt-2 rounded-xl border px-3 py-3"
-              value={value}
-              onChangeText={onChange}
-            ></TextInput>
-          )}
-        ></Controller>
-      </View>
+        errors={errors}
+      ></AddNotes>
       <Button title="Add note" onPress={handleSubmit(onSubmit)}></Button>
     </View>
   );
---
+++ components/addnotes-refactor.tsx
@@ -0,0 +1,62 @@
+import { CaptureEntry } from "@/app/project/[id]/(tabs)/capture/addnotes";
+import {
+  Category,
+  SimpleCategoryDropdown,
+} from "@/components/app-components/simpleCategoryDropdown";
+import { Control, Controller, FieldErrors } from "react-hook-form";
+import { Text, TextInput, View } from "react-native";
+
+type NotesFormProps = {
+  categories: Category[];
+  control: Control<CaptureEntry>;
+  errors: FieldErrors<CaptureEntry>;
+};
+
+export default function AddNotes({
+  categories,
+  control,
+  errors,
+}: NotesFormProps) {
+  return (
+    <View>
+      {errors.categoryId?.message && (
+        <Text className="text-center text-bold text-l text-red-700 font-semibold">
+          {errors.categoryId.message}
+        </Text>
+      )}
+      <Controller
+        control={control}
+        name="categoryId"
+        render={({ field: { onChange, value } }) => (
+          <View style={{ gap: 6 }}>
+            <Text style={{ opacity: 0.8 }}>Category</Text>
+            <SimpleCategoryDropdown
+              categories={categories}
+              valueId={value}
+              onChange={onChange}
+            ></SimpleCategoryDropdown>
+          </View>
+        )}
+      ></Controller>
+      <View>
+        <Text>Notes</Text>
+        {errors.shortDescription?.message && (
+          <Text className="text-center text-bold text-l text-red-700 font-semibold">
+            {errors.shortDescription.message}
+          </Text>
+        )}
+        <Controller
+          control={control}
+          name="shortDescription"
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              className="mt-2 rounded-xl border px-3 py-3"
+              value={value}
+              onChangeText={onChange}
+            ></TextInput>
+          )}
+        ></Controller>
+      </View>
+    </View>
+  );
+}
