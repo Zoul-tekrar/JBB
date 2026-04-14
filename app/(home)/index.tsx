dea0eb45595df0c093ef6df0518517ab717e311d
Zulfu <zoulina801@gmail.com>
2026-04-14 12:54:45 +0100

    Added clerk authentication, just sigin/signup

---
+++ app/(home)/index.tsx
@@ -0,0 +1,96 @@
+import ProjectTileView from "@/components/app-components/project-tile-view";
+import { JbbTitle } from "@/components/design-components/JbbTitle";
+import { colorCodes } from "@/components/ui/colorCodes";
+import { API_BASE_URL } from "@/constants/urls";
+import { ProjectDto } from "@/dtos/dtos";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { router, useFocusEffect } from "expo-router";
+import React, { useCallback, useState } from "react";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function Projects() {
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState<ProjectDto[]>();
+  useFocusEffect(
+    useCallback(() => {
+      const loadProjects = async () => {
+        try {
+          const projects = await getProjects();
+          setProjects(projects);
+        } catch (e) {
+          setError(e instanceof Error ? e.message : "Failed to load projects");
+        } finally {
+          setLoading(false);
+        }
+      };
+      loadProjects();
+    }, []),
+  );
+
+  async function getProjects(): Promise<ProjectDto[]> {
+    const res = await fetch(`${API_BASE_URL}/project`);
+
+    if (!res.ok) {
+      const text = await res.text();
+      throw new Error(`${res.status} : ${text}`);
+    }
+
+    const createdProjects: ProjectDto[] = await res.json();
+
+    console.log(createdProjects);
+
+    return createdProjects;
+  }
+
+  return (
+    <View style={styles.project}>
+      <JbbTitle title="Projects"></JbbTitle>
+
+      {loading && <Text className="text-lg">Loading...</Text>}
+      {error && <Text className="text-red-700 font-semibold">{error}</Text>}
+
+      <FlatList<ProjectDto>
+        data={projects}
+        renderItem={({ item }) => (
+          <ProjectTileView
+            id={item.id.toString()}
+            name={item.name}
+          ></ProjectTileView>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      ></FlatList>
+
+      <Pressable onPress={() => router.push("/addproject")}>
+        <View style={{ ...styles.add_project }}>
+          <View>
+            <AntDesign
+              name="plus-square"
+              size={24}
+              color={colorCodes.JBBORANGE}
+            />
+          </View>
+          <View>
+            <Text style={styles.project_button_text}>Add Project</Text>
+          </View>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  project: {
+    justifyContent: "center",
+    marginHorizontal: 70,
+  },
+
+  add_project: {
+    marginTop: 40,
+    flexDirection: "row",
+  },
+  project_button_text: {
+    marginLeft: 10,
+    fontSize: 22,
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,30 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
 import "../global.css";
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 import { View } from "react-native";
 import Toast from "react-native-toast-message";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ClerkProvider publishableKey={publishableKey!} tokenCache={tokenCache}>
       <View style={{ flex: 1, paddingHorizontal: 20 }}>
         <Stack></Stack>
         <StatusBar style="auto" />
         <Toast />
       </View>
-    </ThemeProvider>
+    </ClerkProvider>
   );
 }
